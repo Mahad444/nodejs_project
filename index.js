8f490f9ad0b48067e6fbbde47ee18615a820fe65
Mahad444 <mahadsaid95@icloud.com>
2022-10-12 14:37:17 +0300

    The index file

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require ('express');
 const routes = require ('./Routes/Api');
 const app = express ();
-app.use(routes);
- app.listen(process.env.port ||4000,()=>{
-    console.log(("Now Listening For Requests On:http://localhost:4000"));
- });
+const bodyParser =require ('body-parser');
+app.use(bodyParser.json()) ; 
+// THIS SHOULD BE ABOVE app.use(routes);
+app.use(routes); 
+app.use((err,req,res,next)=>{
+   res.status(422).send({error:err.message});
+});
+const mongoose =require('mongoose');
+// THIS SHOULD BE ABOVE app.listen
+// This will connect to mongo DB and create studentDb
+app.listen(process.env.port ||4000,()=>{
+   console.log(("Now Listening For Requests On:http://localhost:4000"));
+});
+
+mongoose.connect('mongodb://localhost/studentdb')
 
 
